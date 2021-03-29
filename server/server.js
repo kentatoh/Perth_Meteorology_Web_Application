c604c7a23aaa30f0429ad6b467e9b04c9a4aaeb5
Kenta <kentamalau@gmail.com>
2021-03-30 02:35:59 +0800

    server.js completed

---
+++ server/server.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var http = required("http");
+var url = require("url");
+
+const port = 40541;
+
+function startServer(route, handle) {
+  http
+    .createServer(function (req, res) {
+      var parsedUrl = url.parse(req.url);
+      var path = parsedUrl.pathname;
+
+      console.log("Request for " + path + "...");
+      route(handle, path, req, res);
+    })
+    .listen(port);
+  console.log("Server running on port " + port + " with PID " + process.pid);
+}
+
+exports.startServer = startServer;
