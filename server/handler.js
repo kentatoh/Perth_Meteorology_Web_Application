6808b87abea425214780d6da2c249a7309b44782
Kenta <kentamalau@gmail.com>
2021-03-30 12:10:42 +0800

    fixed server and css output

--- server/handler.js
+++ server/handler.js
@@ -7,7 +7,26 @@ function reqStart(req, res) {
   res.writeHead(200, {
     "Content-Type": "text/html",
   });
-  fs.createReadStream("../html/index.html", "utf-8").pipe(res);
+  fs.createReadStream("../client/index.html", "utf-8").pipe(res);
+}
+
+function indexCss(req, res) {
+  console.log("Index CSS function was called");
+  res.writeHead(200, {
+    "Content-Type": "text/css",
+  });
+  fs.createReadStream("../css/index.css").pipe(res);
+}
+
+function error(req, res) {
+  console.log("Error function was callled");
+  res.writeHead(404, {
+    "Content-Type": "text/html",
+  });
+  res.write("<h1>404 not found</h1>\n");
+  res.end();
 }
 
 exports.reqStart = reqStart;
+exports.indexCss = indexCss;
+exports.error = error;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const srv = require("./server");
-const route = require("./router");
-const handlers = require("./handlers");
+const router = require("./router");
+const handlers = require("./handler");
 
 var handle = {
   "/": handlers.reqStart,
+  "/indexCss": handlers.indexCss,
+  error: handlers.error,
 };
 
 srv.startServer(router.route, handle);
