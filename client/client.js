d56f33464571ff0c0475cd5d143e9dab32426b3c
Kenta <kentamalau@gmail.com>
2021-04-01 01:58:44 +0800

    added create table function

--- client/client.js
+++ client/client.js
@@ -27,8 +27,78 @@ $(function () {
 
       $.post("/search", json, function (data) {
         const objJSON = JSON.parse(data);
-        console.log(data);
+        console.log(objJSON);
+
+        if (inputMeasurement === "table") {
+          $("graph-container").hide();
+          createTable(objJSON, inputOutput);
+        } else if (inputMeasurement === "graph") {
+          $("table-container").hide();
+          createGraph(objJSON, inputStartMonth, inputEndMonth, inputOutput);
+        } else {
+          createTable(objJSON, inputStartMonth, inputEndMonth, inputOutput);
+          createGraph(objJSON, inputStartMonth, inputEndMonth, inputOutput);
+        }
       });
     }
   });
 });
+
+const createTable = (data, startMonth, endMonth, dataType) => {
+  $("table").remove();
+  var table =
+    "<table>" +
+    "<tr>" +
+    "<th></th>" +
+    "<th>Jan</th>" +
+    "<th>Feb</th>" +
+    "<th>Mar</th>" +
+    "<th>Apr</th>" +
+    "<th>May</th>" +
+    "<th>Jun</th>" +
+    "<th>Jul</th>" +
+    "<th>Aug</th>" +
+    "<th>Sep</th>" +
+    "<th>Oct</th>" +
+    "<th>Nov</th>" +
+    "<th>Dec</th>" +
+    "</tr>";
+
+  var windspeed = "";
+  windspeed += "<tr><td>Wind Speed km/h </td>";
+  for (let i = 1; i <= 12; i++) {
+    if (i < startMonth || i > endMonth) {
+      windspeed += "<td><td>";
+    } else {
+      windspeed += `<td>${data.ws[i - 1]}</td>`;
+      console.log(data.ws[i - 1]);
+    }
+  }
+  windspeed += "</tr>";
+
+  var solarradiation = "";
+  solarradiation += "<tr><td>Solar Radiation kWh/m<sup>2</sup></td>";
+  for (let i = 1; i <= 12; i++) {
+    if (i < startMonth || i > endMonth) {
+      solarradiation += "<td><td>";
+    } else {
+      solarradiation += `<td>${data.sr[i - 1]}</td>`;
+      console.log(data.sr[i - 1]);
+    }
+  }
+  solarradiation += "</tr>";
+
+  if (dataType === "windspeed") {
+    table += windspeed;
+  } else if (dataType === "solarradiation") {
+    table += solarradiation;
+  } else {
+    table += windspeed;
+    table += solarradiation;
+  }
+  table += "<table>";
+  console.log(table);
+  $(".table-container").append(table);
+};
+
+const createGraph = () => {};
